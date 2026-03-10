82e48032d241c1cfe9b16a1de119367795438f39
Luki <lukasjagusz6@gmail.com>
2026-03-10 15:31:32 +0100

    add error handling, change return type

--- src/next_station/fetch_population_grid.py
+++ src/next_station/fetch_population_grid.py
@@ -1,20 +1,35 @@
 from .runner import runner
+import jsonpath
 
-def fetch_population_grid(api_url: str) -> str | None:
+def fetch_population_grid(api_url: str,
+                          json_expr: str
+                          ) -> bool:
 
     response = runner(api_url, 'get')
 
     if response is not None:
 
-        file_url = response.json()['data'][-1].get('files')[0]
-        population_grid = runner(file_url, 'get')
+        try:
 
-        
-        if population_grid:
-            
-            with open('population_grid.tif', 'wb') as file:
-                file.write(population_grid.content)
+            file_url = jsonpath.findall(json_expr, response.json())
+            population_grid = runner(*file_url, 'get')
 
-                return 'Success'
+            if population_grid:
+                
+                with open('population_grid.tif', 'wb') as file:
+                    file.write(population_grid.content)
 
-    return None     
+                    return True
+
+
+        except TypeError as err:
+            print(f"List is either empty or got too many arguments \n{err}")
+
+        except OSError as oserr:
+            print(f"Error occurred while writing file on disk \n{oserr}")
+
+        except Exception as ex:
+            print(f"Other error occurred: \n{ex}")
+
+
+    return False
