9a80f2a91b0335be61e4a68e3f24cd0115bcba62
Luki <lukasjagusz6@gmail.com>
2026-03-11 12:04:41 +0100

    add function to compare ETag between two files

---
+++ src/next_station/ingestion/compare_metadata.py
@@ -0,0 +1,33 @@
+from .runner import runner
+import boto3
+from botocore.exceptions import ClientError
+
+def compare_metadata(bucket_name: str,
+                     file_name_on_s3:str,
+                     file_url: str
+                    ) -> bool:
+
+    try:
+
+        api_response = runner(file_url, 'head')
+
+        s3 = boto3.client('s3')
+        aws_response = s3.head_object(
+            Bucket = bucket_name,
+            Key = file_name_on_s3
+        )
+
+        aws_s3_metadata = aws_response['Metadata']['etag']
+        api_metadata = api_response.headers['ETag'].strip('"')
+        
+        if aws_s3_metadata == api_metadata:
+            return False
+
+    
+    except ClientError as e:
+
+        if int(e.response['Error']['Code']) == 404:
+            return True
+
+    
+    return True
