f9c4c41f32dbd443ec0c6e126378ec5d10b42a49
Luki <lukasjagusz6@gmail.com>
2026-03-14 17:47:26 +0100

    add pydantic models and validators for worldpop api response

---
+++ src/next_station/schemas/worldpop.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel, BeforeValidator, Field
+from typing import Annotated
+
+def ensure_one_element(value: any) -> str:
+
+    if isinstance(value, list):
+        
+        if len(value) != 1:
+            raise ValueError(f"Expected one file url, got: {len(value)}.")
+            
+        else:
+            return value[0]
+
+    else:
+        return value
+
+
+
+class FileUrl(BaseModel):
+    file_url: Annotated[str, BeforeValidator(ensure_one_element), Field(alias='files')]
+
+
+
+class GetLastFileUrl(BaseModel):
+    list_url: Annotated[list[FileUrl], Field(alias='data')]
+
+    def __getitem__(self, index):
+        return self.list_url[index].file_url
