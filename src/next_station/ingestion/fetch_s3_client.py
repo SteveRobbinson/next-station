4cee118ae3b88d5ff8c27533190fbbeb71bab391
Luki <lukasjagusz6@gmail.com>
2026-03-15 16:33:30 +0100

    add error handling for 404 status code

--- src/next_station/ingestion/fetch_s3_client.py
+++ src/next_station/ingestion/fetch_s3_client.py
@@ -25,6 +25,9 @@ def fetch_s3_client(bucket_name: str,
         if status_code == '403':
             raise S3AccessDeniedError(f"AWS S3 - Access denied. Status code: {status_code}\nProvided credentials do not have permissions to access S3 resources") from ce
 
+        elif status_code == '404':
+            raise S3NotFoundError(f"AWS S3 - File or bucket not found. Status code: {status_code}") from ce
+
         else:
             raise S3ServiceError(f"AWS S3 - Client Error occurred! Status code: {status_code}") from ce
 
