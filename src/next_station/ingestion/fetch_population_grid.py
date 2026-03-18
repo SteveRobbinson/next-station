efbfdd98680bc1dbcb547f439cd278c8df99951a
Luki <lukasjagusz6@gmail.com>
2026-03-18 15:05:34 +0100

    simplify fetch_population_grid to return response stream

--- src/next_station/ingestion/fetch_population_grid.py
+++ src/next_station/ingestion/fetch_population_grid.py
@@ -1,35 +1,9 @@
 from .runner import runner
-import jsonpath
+import requests
 
 def fetch_population_grid(api_url: str,
-                          json_expr: str
-                          ) -> bool:
+                          is_metadata_same: bool
+                          ) -> requests.Response | None:
 
-    response = runner(api_url, 'get')
-
-    if response is not None:
-
-        try:
-
-            file_url = jsonpath.findall(json_expr, response.json())
-            population_grid = runner(*file_url, 'get')
-
-            if population_grid:
-                
-                with open('population_grid.tif', 'wb') as file:
-                    file.write(population_grid.content)
-
-                    return True
-
-
-        except TypeError as err:
-            print(f"List is either empty or got too many arguments \n{err}")
-
-        except OSError as oserr:
-            print(f"Error occurred while writing file on disk \n{oserr}")
-
-        except Exception as ex:
-            print(f"Other error occurred: \n{ex}")
-
-
-    return False
+    if not is_metadata_same:
+        return runner(api_url, 'get', stream=True)
