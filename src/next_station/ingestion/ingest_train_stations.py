70626633b938f5b6f51ea0c6e0ce0c3889d1b021
Luki <lukasjagusz6@gmail.com>
2026-03-20 18:20:14 +0100

    use create_s3_client helper for s3 client initialization

--- src/next_station/ingestion/ingest_train_stations.py
+++ src/next_station/ingestion/ingest_train_stations.py
@@ -1,9 +1,10 @@
 from src.next_station.ingestion.fetch_train_stations import fetch_train_stations
 from src.next_station.config import settings
 from src.next_station.ingestion.upload_data_to_s3 import upload_data_to_s3
+from src.next_station.ingestion.create_s3_client import create_s3_client
 import boto3
 
-s3 = boto3.client('s3')
+s3 = create_s3_client()
 
 train_stations = fetch_train_stations(settings.train_stations_url,
                                       settings.query_for_train_stations)
