c9212d9d700474c3d09cb8dae25d72ee2d22a1f5
Luki <lukasjagusz6@gmail.com>
2026-04-02 09:44:23 +0200

    update spark session factory to support named databricks profiles

--- src/next_station/core/spark.py
+++ src/next_station/core/spark.py
@@ -1,8 +1,8 @@
 from databricks.connect import DatabricksSession
 from pyspark.sql import SparkSession
 
-def get_spark_session() -> SparkSession:
+def get_spark_session(user: str) -> SparkSession:
 
-    spark = DatabricksSession.builder.getOrCreate()
+    spark = DatabricksSession.builder.profile(user).getOrCreate()
 
     return spark
