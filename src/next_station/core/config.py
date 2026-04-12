be1cd261136626c9e391e52e562d18c4d822f508
Luki <lukasjagusz6@gmail.com>
2026-04-12 16:10:52 +0200

    simplify config and adopt PurePosixPath

--- src/next_station/core/config.py
+++ src/next_station/core/config.py
@@ -1,4 +1,5 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from pathlib import PurePosixPath
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
@@ -6,17 +7,14 @@ class Settings(BaseSettings):
     train_stations_url: str
     population_grid_url: str
     query_for_train_stations: str
-    aws_bucket_name: str
-    aws_railway_stations_file_name: str
-    aws_population_grid_file_name: str
-    aws_population_grid_metadata_file:str
-    aws_s3_bucket_address: str
+    absolute_population_grid_path: PurePosixPath
+    absolute_railway_stations_path: PurePosixPath
     aws_railway_file_explode_by: str
-    databricks_catalog: str
-    databricks_schema: str
-    databricks_railway_table: str
-    databricks_population_table: str
+    absolute_databricks_path: PurePosixPath
+    databricks_railway_stations_table: str
+    databricks_population_grid_table: str
     databricks_sql_user_name: str
     databricks_python_user_name: str
+    aws_s3_protocol: str
 
 settings = Settings()
