e45192f63232020736392edd1b94299d53c83216
Luki <lukasjagusz6@gmail.com>
2026-04-14 19:27:53 +0200

    refactor(jobs): remove logging from railway stations ingestion script

--- src/next_station/jobs/01_api_to_s3_railway_stations.py
+++ src/next_station/jobs/01_api_to_s3_railway_stations.py
@@ -1,26 +1,13 @@
 from src.next_station.providers.fetch_railway_stations import fetch_train_stations
 from src.next_station.core.config import settings
 from src.next_station.infrastructure.s3 import create_s3_client, upload_data_to_s3
-import logging
 
-logger = logging.getLogger(__name__)
-
-logging.basicConfig(
-        level=logging.INFO,
-        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-)
-
-logger.info("Starting train stations ingestion process...")
 s3 = create_s3_client()
 
-logger.info(f"Fetching train stations from API: {settings.train_stations_url}")
 train_stations = fetch_train_stations(settings.train_stations_url,
                                       settings.query_for_train_stations)
 
-logger.info(f"Uploading train stations data to S3 bucket: {settings.aws_bucket_name}")
 upload_data_to_s3(settings.aws_bucket_name,
                   settings.aws_train_file_name,
                   train_stations,
                   s3)
-
-logger.info("Train stations ingestion completed successfully.")
