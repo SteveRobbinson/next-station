3e9ee38429b0f12a48f64f72ff401e07ba145ac9
Luki <lukasjagusz6@gmail.com>
2026-04-16 16:56:44 +0200

    refactor: migrate population grid job to nested configuration

--- src/next_station/jobs/01_api_to_s3_population_grid.py
+++ src/next_station/jobs/01_api_to_s3_population_grid.py
@@ -12,10 +12,10 @@ from src.next_station.infrastructure.slice_dataset import slice_dataset
 
 s3 = create_s3_client()
 
-population_grid_file_url = get_file_url(settings.population_grid_url)
+population_grid_file_url = get_file_url(str(settings.api.base_population_grid_url))
 
 s3_object_metadata = get_s3_object_metadata(s3,
-                                            settings.absolute_population_grid_path.parent)
+                                            settings.aws.s3_bucket_name)
 
 is_metadata_same = compare_metadata(s3_object_metadata, population_grid_file_url)
 
@@ -28,8 +28,8 @@ if not is_metadata_same:
     
     sliced_dataset = slice_dataset(population_grid)
 
-    upload_data_to_s3(str(settings.absolute_population_grid_path.parent),
-                      settings.absolute_population_grid_path.name,
+    upload_data_to_s3(settings.aws.s3_bucket_name,
+                      settings.aws.s3_population_grid_file_name,
                       sliced_dataset,
                       s3,
                       metadata)
