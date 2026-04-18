7c8b0637dbc12a64ee01210a1759eebab971af38
Luki <lukasjagusz6@gmail.com>
2026-04-18 12:43:11 +0200

    refactor: simplify client intialization by using factory pattern

--- src/next_station/infrastructure/s3.py
+++ src/next_station/infrastructure/s3.py
@@ -1,22 +1,15 @@
 import requests
-import logging
 import json
 import boto3
 from mypy_boto3_s3 import S3Client
 from botocore.exceptions import (
-    NoCredentialsError,
-    PartialCredentialsError,
-    NoRegionError,
-    EndpointConnectionError,
-    ClientError
-)
+        BotoCoreError,
+        ClientError
+        )
 
 from src.next_station.core.exceptions import (
     S3ServiceError,
-    S3ConfigError,
-    S3ConnectionError,
-    S3AccessDeniedError,
-    S3NotFoundError
+    S3AccessDeniedError
 )
 
 from .runner import runner
@@ -32,19 +25,9 @@ def create_s3_client() -> S3Client:
         
         return boto3.client('s3')
     
-    except (NoCredentialsError, PartialCredentialsError) as nce:
-        raise S3ConfigError(f"AWS S3 - No credentials found. Check your AWS_ACCESS_KEY_ID and/or AWS_SECRET_ACCESS_KEY") from nce
+    except Exception as err:
+        raise S3ServiceError.from_exception(err) from err
 
-
-    except NoRegionError as nre:
-        raise S3ConfigError(f"AWS S3 - Region for client S3 not specified!") from nre
-
-
-    except EndpointConnectionError as ece:
-        raise S3ConnectionError(f"AWS S3 - Could not connect to the endpoint. Please check your internet or AWS service status") from ece
-
-
-logger = logging.getLogger(__name__)
 
 def get_s3_object_metadata(s3client: S3Client,
                            aws_s3_path: str,
