f96a1cead9b9495a332676123108c1ef2bcfce54
Luki <lukasjagusz6@gmail.com>
2026-04-24 18:04:04 +0200

    refactor: standardize AWS S3 URI generation and fix type hints

--- src/next_station/core/config.py
+++ src/next_station/core/config.py
@@ -51,16 +51,27 @@ class AWSConfig(BaseModel):
     s3_railway_stations_file_name: str
     s3_population_grid_file_name: str
     railway_file_explode_by: str = 'elements'
+    s3_public_prefix: str = 'public'
 
-    @property
-    @computed_field
+    @computed_field # type: ignore[prop-decorator]
+    @property   
     def railway_stations_uri(self) -> str:
         return f"{self.s3_protocol}{self.s3_bucket_name}/{self.s3_railway_stations_file_name}"
 
+    @computed_field # type: ignore[prop-decorator]
     @property
-    @computed_field
     def population_grid_uri(self) -> str:
         return f"{self.s3_protocol}{self.s3_bucket_name}/{self.s3_population_grid_file_name}"
+
+    @computed_field # type: ignore[prop-decorator]
+    @property
+    def population_grid_public(self) -> str:
+        return f"{self.s3_protocol}{self.s3_bucket_name}/{self.s3_public_prefix}/{self.s3_population_grid_file_name}"
+
+    @computed_field # type: ignore[prop-decorator]
+    @property
+    def railway_stations_public(self) -> str:
+        return f"{self.s3_protocol}{self.s3_bucket_name}/{self.s3_public_prefix}/{self.s3_railway_stations_file_name}"
 
 
 class ApiRequestsConfig(BaseModel):
