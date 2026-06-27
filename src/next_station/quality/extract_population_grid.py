371cf6f78a82114221bf7c9c440a924998bbc395
Luki <lukasjagusz6@gmail.com>
2026-06-27 16:06:37 +0200

    refactor: remove fixed repartition from population extraction

--- src/next_station/quality/extract_population_grid.py
+++ src/next_station/quality/extract_population_grid.py
@@ -1,20 +1,20 @@
 from pyspark.sql import DataFrame
 
-def extract_population_points(df: DataFrame,
-                              num_partitions: int = 8) -> DataFrame:
+def extract_population_points(df: DataFrame) -> DataFrame:
     
     df_with_raster = df.selectExpr('RS_FromGeoTiff(content) as raster')
     
     df_tiled = df_with_raster.selectExpr('RS_Tile(raster, 256, 256) as tiles')
 
-    df_distributed = df_tiled.selectExpr('explode(tiles) as tile').repartition(num_partitions)
+    df_distributed = df_tiled.selectExpr('explode(tiles) as tile')
 
-    df_exploded = df_distributed.selectExpr(
-        'explode(RS_PixelAsCentroids(tile, 1)) as exploded'
-    ).selectExpr(
-            'ST_AsBinary(exploded.geom) as geom_wkb',
-            'exploded.value as value'
-    )
+    df_exploded = (df_distributed
+                   .selectExpr(
+                       'explode(RS_PixelAsCentroids(tile, 1)) as exploded')
+                   .selectExpr(
+                       'ST_AsBinary(exploded.geom) as geom_wkb',
+                       'exploded.value as value'
+                       )
+                   )
 
     return df_exploded
-
