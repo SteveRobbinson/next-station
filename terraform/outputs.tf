c9559c7241bb20f0607617a692efff361af88b95
Luki <lukasjagusz6@gmail.com>
2026-07-17 16:55:03 +0200

    feat(terraform): add outputs for workspace and network resources

---
+++ terraform/outputs.tf
@@ -0,0 +1,27 @@
+output "workspace_url" {
+  value = databricks_mws_workspaces.this.workspace_url
+}
+
+output "workspace_id" {
+  value = databricks_mws_workspaces.this.workspace_id
+}
+
+output "vpc_id" {
+  value = aws_vpc.databricks_vpc.id
+}
+
+output "public_subnet_id" {
+  value = aws_subnet.public_subnet.id
+}
+
+output "private_subnet_ids" {
+  value = [aws_subnet.private_subnet_a.id, aws_subnet.private_subnet_b.id]
+}
+
+output "nat_gateway_ip" {
+  value = aws_eip.nat_eip.public_ip
+}
+
+output "cross_account_role_arn" {
+  value = aws_iam_role.cross_account.arn
+}
