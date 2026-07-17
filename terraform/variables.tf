f8cbdce11ee998a06829061d6a1ec584e51418b5
Luki <lukasjagusz6@gmail.com>
2026-07-17 16:41:22 +0200

    style: format variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,49 +1,49 @@
 variable "databricks_account_id" {
-  type        = string
+  type = string
 }
 
 variable "databricks_client_id" {
-  type        = string
+  type = string
 }
 
 variable "databricks_client_secret" {
-  type        = string
-  sensitive   = true
+  type      = string
+  sensitive = true
 }
 
 variable "aws_region" {
-  type        = string
-  default     = "eu-central-1"
+  type    = string
+  default = "eu-central-1"
 }
 
 variable "workspace_name" {
-  type        = string
+  type = string
 }
 
 variable "aws_account_id" {
-  type        = string
+  type = string
 }
 
 variable "cross_account_role_name" {
-  type        = string
+  type = string
 }
 
 variable "root_bucket_name" {
-  type        = string
+  type = string
 }
 
 variable "vpc_cidr" {
-  type        = string
+  type = string
 }
 
 variable "public_subnet_cidr" {
-  type        = string
+  type = string
 }
 
 variable "private_subnet_a_cidr" {
-  type        = string
+  type = string
 }
 
 variable "private_subnet_b_cidr" {
-  type        = string
+  type = string
 }
