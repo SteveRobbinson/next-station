8a738c31d4a749ca3c0a991a8978fd25c3cb2bd4
Luki <lukasjagusz6@gmail.com>
2026-07-18 14:48:04 +0200

    chore(core): add terraform providers configuration

---
+++ terraform/core/providers.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.54.0"
+    }
+    databricks = {
+      source  = "databricks/databricks"
+      version = "~> 1.121.0"
+    }
+  }
+  required_version = ">= 1.5.0"
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+provider "databricks" {
+  alias         = "mws"
+  host          = "https://accounts.cloud.databricks.com"
+  account_id    = var.databricks_account_id
+  client_id     = var.databricks_client_id
+  client_secret = var.databricks_client_secret
+}
