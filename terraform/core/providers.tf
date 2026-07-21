55821d63738db780c5d3bb8d6fa7cb8367c80746
Luki <lukasjagusz6@gmail.com>
2026-07-21 14:54:11 +0200

    feat(terraform): add workspace-level Databricks provider

--- terraform/core/providers.tf
+++ terraform/core/providers.tf
@@ -23,3 +23,10 @@ provider "databricks" {
   client_id     = var.databricks_client_id
   client_secret = var.databricks_client_secret
 }
+
+provider "databricks" {
+  alias         = "workspace"
+  host          = databricks_mws_workspaces.this.workspace_url
+  client_id     = var.databricks_client_id
+  client_secret = var.databricks_client_secret
+}
