eb4fd8166f6cca56392e9ee378b8743f77c559e5
Luki <lukasjagusz6@gmail.com>
2026-07-23 10:12:44 +0200

    chore: add Databricks and AWS provider configurations

---
+++ terraform/core/workspace/providers.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.54.0"
+    }
+    databricks = {
+      source  = "databricks/databricks"
+      version = "~> 1.121.0"
+    }
+  }
+  required_version = ">= 1.5.0"
+}
+
+provider "databricks" {
+  alias         = "mws"
+  host          = "https://accounts.cloud.databricks.com"
+  account_id    = var.databricks_account_id
+  client_id     = var.databricks_client_id
+  client_secret = var.databricks_client_secret
+}
+
+provider "databricks" {
+  alias         = "workspace"
+  host          = databricks_mws_workspaces.this.workspace_url
+  client_id     = var.databricks_client_id
+  client_secret = var.databricks_client_secret
+}
