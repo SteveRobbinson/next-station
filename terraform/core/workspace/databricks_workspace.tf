f92db166a530f13b3dc0e6c760c3880e0431e6ae
Luki <lukasjagusz6@gmail.com>
2026-07-28 12:15:14 +0200

    feat(terraform): add github actions oidc federation policy

--- terraform/core/workspace/databricks_workspace.tf
+++ terraform/core/workspace/databricks_workspace.tf
@@ -25,3 +25,12 @@ resource "databricks_service_principal" "github_actions" {
   display_name = var.gh_actions_service_principal
 }
 
+resource "databricks_service_principal_federation_policy" "this" {
+  provider             = databricks.mws
+  service_principal_id = databricks_service_principal.github_actions.id
+  policy_id            = "github-actions-policy"
+  oidc_policy = {
+    "issuer" : "https://token.actions.githubusercontent.com",
+    "subject" : "repo:${var.github_org}/${var.github_repo}:environment:prod"
+  }
+}
