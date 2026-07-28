ce34338017552e15c7422094768c00fa36d90ddc
Luki <lukasjagusz6@gmail.com>
2026-07-28 11:59:51 +0200

    refactor(terraform): remove unused databricks credentials variables

--- terraform/core/infrastructure/variables.tf
+++ terraform/core/infrastructure/variables.tf
@@ -6,15 +6,6 @@ variable "databricks_account_id" {
   type = string
 }
 
-variable "databricks_client_id" {
-  type = string
-}
-
-variable "databricks_client_secret" {
-  type      = string
-  sensitive = true
-}
-
 variable "aws_region" {
   type    = string
   default = "eu-central-1"
