244700c9279261c43295b11889cd0f1f9ca969b9
Luki <lukasjagusz6@gmail.com>
2026-07-28 11:58:51 +0200

    refactor(terraform): update workspace variables for service principal

--- terraform/core/workspace/variables.tf
+++ terraform/core/workspace/variables.tf
@@ -2,20 +2,23 @@ variable "databricks_account_id" {
   type = string
 }
 
-variable "databricks_client_id" {
-  type = string
-}
-
-variable "databricks_client_secret" {
-  type      = string
-  sensitive = true
-}
-
 variable "aws_region" {
   type    = string
   default = "eu-central-1"
 }
 
 variable "workspace_name" {
+  type = string
+}
+
+variable "gh_actions_service_principal" {
+  type = string
+}
+
+variable "github_org" {
+  type = string
+}
+
+variable "github_repo" {
   type = string
 }
