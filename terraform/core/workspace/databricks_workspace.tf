117f719b2f2dec163836b67baacb4e7eaaf5ed63
Luki <lukasjagusz6@gmail.com>
2026-07-30 16:13:40 +0200

    refactor(terraform): remove unused service principal resources

--- terraform/core/workspace/databricks_workspace.tf
+++ terraform/core/workspace/databricks_workspace.tf
@@ -20,20 +20,6 @@ resource "databricks_mws_permission_assignment" "add_user" {
   permissions  = ["ADMIN"]
 }
 
-resource "databricks_service_principal" "github_actions" {
-  provider     = databricks.mws
-  display_name = var.gh_actions_service_principal
-}
-
-resource "databricks_service_principal_federation_policy" "this" {
-  provider             = databricks.mws
-  service_principal_id = databricks_service_principal.github_actions.id
-  policy_id            = "github-actions-policy"
-  oidc_policy = {
-    "issuer" : "https://token.actions.githubusercontent.com",
-    "subject" : "repo:${var.github_org}/${var.github_repo}:environment:prod"
-  }
-}
 
 resource "databricks_mws_permission_assignment" "add_gh_actions_sp" {
   provider     = databricks.mws
