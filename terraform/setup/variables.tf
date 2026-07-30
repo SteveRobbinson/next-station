d8e804a72f4a3edc3d354ac7f62a11c5c8d38be9
Luki <lukasjagusz6@gmail.com>
2026-07-30 16:38:15 +0200

    feat(terraform): add input variables to setup

---
+++ terraform/setup/variables.tf
@@ -0,0 +1,14 @@
+variable "gh_actions_service_principal" {
+  type    = string
+  default = "github-actions-deployer"
+}
+
+variable "github_org" {
+  type    = string
+  default = "SteveRobbinson"
+}
+
+variable "github_repo" {
+  type    = string
+  default = "next-station"
+}
