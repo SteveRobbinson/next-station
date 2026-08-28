0b014aae574c7b771166569532bf0151b8b0b884
Luki <lukasjagusz6@gmail.com>
2026-08-28 13:06:58 +0200

    test(s3): verify S3Manager initialization

--- tests/next_station/infrastructure/test_s3.py
+++ tests/next_station/infrastructure/test_s3.py
@@ -2,6 +2,8 @@ import pytest
 import boto3
 from moto import mock_aws
 
+from next_station.infrastructure.s3 import S3Manager
+
 
 @pytest.fixture
 def mock_s3_env():
@@ -10,3 +12,11 @@ def mock_s3_env():
         bucket_name = 'test_bucket_name'
         s3.create_bucket(Bucket=bucket_name)
         yield bucket_name
+
+
+def test_s3manager_init_success(mock_s3_env):
+
+    manager = S3Manager(aws_s3_bucket_name=mock_s3_env)
+
+    assert manager.aws_s3_bucket_name == mock_s3_env
+    assert manager.s3 is not None
