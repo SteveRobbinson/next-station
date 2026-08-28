a28903f8e688403d8f11d1e04db44b8750f5d4de
Luki <lukasjagusz6@gmail.com>
2026-08-28 17:30:11 +0200

    feat(spark): add read_from_s3 supporting json and binaryFile reads

--- src/next_station/infrastructure/spark.py
+++ src/next_station/infrastructure/spark.py
@@ -1,6 +1,7 @@
 import logging
+from typing import Literal
 
-from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession, DataFrame
 
 
 logger = logging.getLogger(__name__)
@@ -14,6 +15,27 @@ class SparkManager:
             self.spark.range(1).count()
 
             logger.info("Successfully intialized spark session")
+
+        except Exception as err:
+            raise RuntimeError() from err
+
+
+    def read_from_s3(self,
+                     aws_s3_path: str,
+                     data_format: Literal['json', 'binaryFile']
+                     ) -> DataFrame:
+
+        logger.info(f"Started fetching data from {aws_s3_path}")
+
+        try:
+            if data_format == 'json':
+                df = self.spark.read.format(data_format).option('multiLine', 'true').load(aws_s3_path)
+
+            elif data_format == 'binaryFile':
+                df = self.spark.read.format(data_format).load(aws_s3_path)
+
+            logger.info(f"Successfully loaded data from {aws_s3_path} to spark")
+            return df
 
         except Exception as err:
             raise RuntimeError() from err
