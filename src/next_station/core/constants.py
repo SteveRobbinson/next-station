2f995f134f538643d5ef9a09381f9b16f96c6416
Luki <lukasjagusz6@gmail.com>
2026-08-29 15:49:12 +0200

    refactor: introduce new custom exceptions for S3

--- src/next_station/core/constants.py
+++
@@ -1,8 +0,0 @@
-from enum import StrEnum
-
-PACKAGE_NAME = 'next_station.core.data'
-ERROR_MAPPING_FILE = 'error_mappings.json'
-
-class ErrorCategory(StrEnum):
-    STATUS_DESC = 'status_description'
-    AWS_TO_HTTP = 'aws_error_to_status'
