b2cda265b8801d8f32eccb183c4d7aafaa8e8840
Luki <lukasjagusz6@gmail.com>
2026-08-29 13:52:53 +0200

    test(spark): verify save_df_in_databricks api calls

--- tests/next_station/infrastructure/test_spark.py
+++ tests/next_station/infrastructure/test_spark.py
@@ -1,4 +1,5 @@
 import json
+from unittest.mock import MagicMock
 
 from next_station.infrastructure.spark import SparkManager
 
@@ -23,3 +24,21 @@ def test_verify_json_reads(tmp_path):
     spark_read = spark.read_from_s3(aws_s3_path=str(file_path), data_format='json').first().asDict()
     
     assert spark_read == test_data
+
+
+def test_save_df_in_databricks():
+    mock_df = MagicMock()
+    table_name = "silver.test_data"
+    
+    SparkManager.save_df_in_databricks(
+        df=mock_df,
+        table_name=table_name,
+        save_format="delta",
+        save_mode="overwrite",
+        merge_schema=True
+    )
+
+    mock_df.write.format.assert_called_once_with("delta")
+    mock_df.write.format().mode.assert_called_once_with("overwrite")
+    mock_df.write.format().mode().option.assert_called_once_with("mergeSchema", True)
+    mock_df.write.format().mode().option().saveAsTable.assert_called_once_with(table_name)
