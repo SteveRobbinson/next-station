8c1592fcd809e4a4bf1cf12837f4d1f73e128a94
Luki <lukasjagusz6@gmail.com>
2026-08-30 19:37:16 +0200

    chore(spark): move extract_population_points to spark_helper.py

--- src/next_station/transformation/spark_helpers.py
+++ src/next_station/transformation/spark_helpers.py
@@ -7,3 +7,21 @@ def explode_and_flatten(df: DataFrame,
     
     df_long = df.select(explode(explode_by).alias('node'))
     return df_long.select('node.*')
+
+
+def extract_population_points(df: DataFrame) -> DataFrame:
+    
+    df_with_raster = df.selectExpr('RS_FromGeoTiff(content) as raster')
+    df_tiled = df_with_raster.selectExpr('RS_Tile(raster, 256, 256) as tiles')
+    df_distributed = df_tiled.selectExpr('explode(tiles) as tile')
+
+    df_exploded = (df_distributed
+                   .selectExpr(
+                       'explode(RS_PixelAsCentroids(tile, 1)) as exploded')
+                   .selectExpr(
+                       'ST_AsBinary(exploded.geom) as geom_wkb',
+                       'exploded.value as value'
+                       )
+                   )
+
+    return df_exploded
