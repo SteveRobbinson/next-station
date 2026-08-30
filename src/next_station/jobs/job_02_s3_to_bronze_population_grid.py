cf6329a29feea0c235ff3dc17ed60c853ddf6ad7
Luki <lukasjagusz6@gmail.com>
2026-08-30 19:21:33 +0200

    refactor(jobs): redesign population_grid_to_databricks job to match new project architecture

--- src/next_station/jobs/job_02_s3_to_bronze_population_grid.py
+++ src/next_station/jobs/job_02_s3_to_bronze_population_grid.py
@@ -1,11 +1,10 @@
 import logging
-from next_station.core.spark import get_spark_session
-from next_station.infrastructure.load_population_grid import load_population_grid
-from next_station.core.config.settings import settings
-from next_station.infrastructure.databricks import save_df_in_db
+
 from sedona.spark import SedonaContext
+
+from next_station.infrastructure.spark import SparkManager
+from next_station.core.config.settings import settings
 from next_station.quality.extract_population_grid import extract_population_points
-from next_station.core.exceptions.base import BaseAppError, InfrastructureError, UnifiedAPIError
 
 logger = logging.getLogger(__name__)
 
@@ -14,37 +13,23 @@ def load_population_grid_to_databricks():
 
     try:
 
-        spark_session = get_spark_session()
-        spark_session = SedonaContext.create(spark_session)
+        spark = SparkManager()
+        spark_session = SedonaContext.create(spark.spark)
 
         logger.info(f"Ingesting raw data from {settings.aws.population_grid_uri}")
-        raw_df = load_population_grid(spark_session, settings.aws.population_grid_uri)
+        raw_df = spark.read_from_s3(aws_s3_path=settings.aws.population_grid_uri, data_format='binaryFile')
 
         logger.info("Extracting population points from raw dataset")
         df_exploded = extract_population_points(raw_df)
     
         logger.info(f"Persisting exploded dataset to table: {settings.databricks.population_grid_bronze_fqn}")
-        save_df_in_db(df_exploded, settings.databricks.population_grid_bronze_fqn)
+        spark.save_df_in_databricks(df_exploded, settings.databricks.population_grid_bronze_fqn)
 
         logger.info("Successfully completed population grid load to Databricks")    
 
-
-    except (InfrastructureError, UnifiedAPIError) as known_err:
-
-        logger.error(f"Job failed due to error in {known_err.source}: {known_err.details}")
+    except Exception:
+        logger.error("An error occurred while processing and uploading data to databricks")
         raise
-
-
-    except Exception as err:
-
-        msg = "Unexpected failure during Databricks population grid ingestion"
-        logger.exception(msg)
-        raise BaseAppError(
-            source="PopulationGridDatabricksLoader",
-            status_code=500,
-            details=f"{msg}: {str(err)}"
-        ) from err
-
 
 if __name__ == '__main__':
     load_population_grid_to_databricks()
