cd6399aa7c613f72502a764e5b9799de02d953f2
Luki <lukasjagusz6@gmail.com>
2026-08-30 19:47:45 +0200

    refactor(jobs): redesign job pipeline to meet new project requirements

--- src/next_station/jobs/job_02_s3_to_bronze_railway_stations.py
+++ src/next_station/jobs/job_02_s3_to_bronze_railway_stations.py
@@ -1,11 +1,8 @@
 import logging
-from next_station.core.spark import get_spark_session
-from next_station.infrastructure.load_railway_stations import load_json_source
-from next_station.quality.df_empty import is_df_empty
-from next_station.quality.melt_table import melt_table
+
+from next_station.infrastructure.spark import SparkManager
+from next_station.transformations.spark_helpers import explode_and_flatten
 from next_station.core.config.settings import settings
-from next_station.infrastructure.databricks import save_df_in_db
-from next_station.core.exceptions.base import BaseAppError, InfrastructureError, UnifiedAPIError
 
 logger = logging.getLogger(__name__)
 
@@ -13,40 +10,23 @@ def load_railway_stations_to_databricks():
     logger.info('Initiating railway stations load to Databricks Bronze layer')
 
     try:
-
-        spark_session = get_spark_session()
+        spark = SparkManager()
 
         logger.info(f"Ingesting raw data from {settings.aws.railway_stations_uri}")
-        df = load_json_source(spark_session, settings.aws.railway_stations_uri)
+        df_raw = spark.read_from_s3(aws_s3_path=settings.aws.railway_stations_uri, data_format='json')
 
-        logger.info(f"Exploding table by {settings.aws.railway_file_explode_by}")
-        df = melt_table(df, settings.aws.railway_file_explode_by)
+        logger.info("Table transformations")
+        df_processed = explode_and_flatten(df=df_raw, explode_by=settings.aws.railway_file_explode_by)
 
-        logger.info("Integrity checks...")
-        df = is_df_empty(df)
-
-        logger.info(f"Persisting dataset to table {settings.databricks.railway_stations_bronze_fqn}")
-        save_df_in_db(df, settings.databricks.railway_stations_bronze_fqn, merge_schema = True)
+        logger.info("Uploading table to databricks")
+        spark.save_df_in_databricks(df=df_processed,
+                                    table_name=settings.databricks.railway_stations_bronze_fqn)
 
         logger.info("Successfully loaded railway stations to Databricks")
 
-
-    except (InfrastructureError, UnifiedAPIError) as known_err:
-
-            logger.error(f"Job failed due to error in {known_err.source}: {known_err.details}")
-            raise
-
-
-    except Exception as err:
-
-        msg = "Unexpected failure during Databricks railway stations ingestion"
-        logger.exception(msg)
-        raise BaseAppError(
-            source="### RailwayStationsDatabricksLoader ###",
-            status_code=500,
-            details=f"{msg}: {str(err)}"
-        ) from err
-
+    except Exception:
+        logger.error("An error occurred while uploading railway stations to databricks")
+        raise
 
 if __name__ == '__main__':
     load_railway_stations_to_databricks()
