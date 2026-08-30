e8dfcd1688404496176f0b7332ff2d8683158ae3
Luki <lukasjagusz6@gmail.com>
2026-08-30 13:24:17 +0200

    feat: add helper function to parse api object metadata

--- src/next_station/infrastructure/utils.py
+++ src/next_station/infrastructure/utils.py
@@ -2,6 +2,11 @@ import logging
 import sys
 import time
 
+from pydantic import ValidationError
+
+from next_station.infrastructure.runner import runner
+from next_station.schemas.worldpop import ApiMetadata
+
 logger = logging.getLogger(__name__)
 
 def _perform_backoff(current_retry_count: int,
@@ -22,3 +27,14 @@ def setup_logging() -> None:
         handlers=[logging.StreamHandler(sys.stdout)],
         force=True
         )
+
+def get_api_object_metadata(file_url: str) -> str:
+    try:
+        api_response = runner(file_url, 'head')
+        api_metadata = ApiMetadata(**api_response.headers).etag
+
+        return api_metadata
+
+    except ValidationError as err:
+        logger.error(f"Parsing metadata from {file_url} failed, {err.errors()}")
+        raise
