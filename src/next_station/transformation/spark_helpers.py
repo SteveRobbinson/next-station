0b0cea1ef24adb77aaf541e1409d551e322569f2
Luki <lukasjagusz6@gmail.com>
2026-08-30 19:35:26 +0200

    refactor(spark): rename melt_table to explode_and_flatten

--- src/next_station/transformation/spark_helpers.py
+++ src/next_station/transformation/spark_helpers.py
@@ -1,10 +1,9 @@
 from pyspark.sql import DataFrame
 from pyspark.sql.functions import explode
 
-def melt_table(df: DataFrame,
-               explode_by: str
-               ) -> DataFrame:
+def explode_and_flatten(df: DataFrame,
+                        explode_by: str
+                        ) -> DataFrame:
     
     df_long = df.select(explode(explode_by).alias('node'))
-
     return df_long.select('node.*')
