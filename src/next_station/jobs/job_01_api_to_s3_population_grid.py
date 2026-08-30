ff3c9425b9a2b28341b02a2e479686745fd7056d
Luki <lukasjagusz6@gmail.com>
2026-08-30 18:18:50 +0200

    feat(jobs): redesign railway stations and population grid jobs - integrate S3Manager - replace runner wrappers with direct runner calls - simplify error handling logic - organize imports

--- src/next_station/jobs/job_01_api_to_s3_population_grid.py
+++ src/next_station/jobs/job_01_api_to_s3_population_grid.py
@@ -1,15 +1,10 @@
 import logging
-from next_station.infrastructure.s3 import (
-    create_s3_client,
-    get_s3_object_metadata,
-    compare_metadata,
-    upload_data_to_s3
-)
+
+from next_station.infrastructure.s3 import S3Manager
 from next_station.core.config.settings import settings
-from next_station.schemas.worldpop import ApiMetadata
 from next_station.providers.get_file_url import get_file_url
-from next_station.providers.fetch_population_grid import fetch_population_grid
-from next_station.core.exceptions.base import BaseAppError, InfrastructureError, UnifiedAPIError
+from next_station.infrastructure.utils import get_api_object_metadata
+from next_station.infrastructure.runner import runner
 
 logger = logging.getLogger(__name__)
 
@@ -18,46 +13,30 @@ def ingest_population_grid_to_s3():
 
     try:
 
-        s3 = create_s3_client()
+        s3 = S3Manager(settings.aws.s3_bucket_name)
         population_grid_file_url = get_file_url(str(settings.api.base_population_grid_url))
 
         logger.info(f"Checking for updates at: {population_grid_file_url}")
-        s3_object_metadata = get_s3_object_metadata(s3, settings.aws.s3_bucket_name, f"{settings.aws.s3_population_grid_file_name}/metadata.json")
-        is_metadata_same = compare_metadata(s3_object_metadata, population_grid_file_url)
+        s3_object_metadata = s3.get_object_metadata(file_path=settings.aws.s3_population_grid_file_name)
+        api_object_metadata = get_api_object_metadata(population_grid_file_url)
 
-        if not is_metadata_same:
+        if s3_object_metadata != api_object_metadata:
             logger.info('Change detected. Fetching and processing new population grid...')
-            population_grid = fetch_population_grid(population_grid_file_url)
-            metadata = {'ETag': ApiMetadata(**population_grid.headers).etag}
 
-            upload_data_to_s3(settings.aws.s3_bucket_name,
-                              settings.aws.s3_population_grid_file_name,
-                              population_grid.raw,
-                              s3,
-                              metadata)
+            population_grid = runner(api_url=population_grid_file_url, method='get', stream=True)
+
+            s3.upload_data_to_s3(file_name=settings.aws.s3_population_grid_file_name,
+                                 object_to_upload=population_grid.raw,
+                                 metadata=api_object_metadata)
 
             logger.info('Successfully updated population grid in S3.')
 
         else:
             logger.info('Metadata is identical. Skipping update to save resources.')
 
-
-    except (UnifiedAPIError, InfrastructureError) as known_err:
-
-            logger.error(f"Job failed due to known error in {known_err.source}: {known_err.details}")
-            raise
-
-
-    except Exception as err:
-
-        msg = "Unexpected failure in Population Grid sync job"
-        logger.exception(msg)
-        raise BaseAppError(
-            source="### PopulationGridJob ###",
-            status_code=500,
-            details=f"{msg}: {str(err)}"
-        ) from err
-
+    except Exception:
+        logger.exception("An UNEXPECTED error occurred, job failed")
+        raise
 
 if __name__ == "__main__":
     ingest_population_grid_to_s3()
